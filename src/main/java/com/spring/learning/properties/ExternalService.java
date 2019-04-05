188fd8b57996ab4da51ae3a9a7fb5102b38ad490
Rajeev Gurram <rgurram@paloaltonetworks.com>
2019-04-05 12:39:49 -0700

    loading data from property file url.properties with @Value, @ProeprtySource

--- src/main/java/com/spring/learning/properties/ExternalService.java
+++ src/main/java/com/spring/learning/properties/ExternalService.java
@@ -1,9 +1,9 @@
 package com.spring.learning.properties;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Service;
+import org.springframework.stereotype.Component;
 
-@Service
+@Component
 public class ExternalService {
     @Value("${service.url}")
     private String serviceUrl;
